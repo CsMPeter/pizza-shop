1b4c24e36a3c209d1a0c81a941ec5f6801435546
CsMPeter <csurkampeter@gmail.com>
2020-01-24 13:55:00 +0200

    negative flow for create

--- src/test/java/org/fasttrackit/pizzashop/PizzaServiceTests.java
+++ src/test/java/org/fasttrackit/pizzashop/PizzaServiceTests.java
@@ -3,26 +3,31 @@ package org.fasttrackit.pizzashop;
 import org.fasttrackit.pizzashop.domain.Pizza;
 import org.fasttrackit.pizzashop.service.PizzaService;
 import org.fasttrackit.pizzashop.transfer.SavePizzaRequest;
-import org.junit.jupiter.api.Test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.TransactionSystemException;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.greaterThan;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class PizzaServiceTests {
+public class PizzaServiceTests {
 
 	@Autowired
 	private PizzaService pizzaService;
 
 	@Test
-	void testCreatePizza_whenValidRequest_thenProductIsSaved() {
+	public void testCreatePizza_whenValidRequest_thenProductIsSaved() {
 
 		SavePizzaRequest request = new SavePizzaRequest();
-		request.setName("Diavola");
+		request.setName("Diavola" + System.currentTimeMillis());
 		request.setPrice(20.0);
 		request.setIngredients("mozarella, tomato sauce, pepperoni");
 
@@ -37,6 +42,14 @@ class PizzaServiceTests {
 		assertThat(createdPizza.getPrice(),is(request.getPrice()));
 
 
+	}
+
+	@Test(expected = TransactionSystemException.class)
+	public void testCreateProduct_whenInvalidRequest_thenThrowException(){
+		SavePizzaRequest request = new SavePizzaRequest();
+		//leaving request properties with null values tovalidate the negative flow
+
+		pizzaService.createPizza(request);
 	}
 
 }
